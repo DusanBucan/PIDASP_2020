037d9eb9eb0dfe611e83be09cbd6be2b87356c68
DusanBucan <dusanbzr@gmail.com>
2021-02-11 17:07:58 +0100

    configured network

---
+++ test-network/all.sh
@@ -0,0 +1,23 @@
+export COMPOSE_PROJECT_NAME="project1"
+# ./network.sh down
+# ./network.sh up -i 2.3.1 -cai 2.3.1
+# ./network.sh createChannel
+# ./network.sh deployCC -ccn basic -ccl go -ccp ../asset-transfer-basic/chaincode-go
+
+
+export PATH=${PWD}/../bin:$PATH
+export FABRIC_CFG_PATH=$PWD/../config/
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID="Org1MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+export CORE_PEER_ADDRESS=localhost:7051
+
+
+# peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C mychannel -n basic --peerAddresses localhost:7051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt" --peerAddresses localhost:10051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt" --peerAddresses localhost:13051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls/ca.crt" -c '{"function":"InitLedger","Args":[]}'
+
+#ovo je za query
+peer chaincode query -C mychannel -n basic -c '{"Args":["GetAllAssets"]}'
+
+#za prenos dobara (ovde fora navedes sva tree anchore peer-a onda prolazi, sta se desi ako stavis 2 anchore peer-a?)
+# peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C mychannel -n basic --peerAddresses localhost:7051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt" --peerAddresses localhost:10051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt" --peerAddresses localhost:13051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls/ca.crt" -c '{"function":"TransferAsset","Args":["asset6","Dusan"]}'
---
+++ test-network/scripts/envVar.sh
@@ -0,0 +1,141 @@
+#!/bin/bash
+#
+# Copyright IBM Corp All Rights Reserved
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# This is a collection of bash functions used by different scripts
+
+# imports
+. scripts/utils.sh
+
+export CORE_PEER_TLS_ENABLED=true
+export ORDERER_CA=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+export PEER0_ORG1_CA=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export PEER0_ORG2_CA=${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+export PEER0_ORG3_CA=${PWD}/organizations/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls/ca.crt
+export ORDERER_ADMIN_TLS_SIGN_CERT=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/tls/server.crt
+export ORDERER_ADMIN_TLS_PRIVATE_KEY=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/tls/server.key
+
+# Set environment variables for the peer org
+setGlobals() {
+  local USING_ORG=""
+  local USING_PEER_IN_ORG=""
+  if [ -z "$OVERRIDE_ORG" ]; then
+    USING_ORG=$1
+  else
+    USING_ORG="${OVERRIDE_ORG}"
+  fi
+
+# podesavanja na koji peer ce da ukazuje
+  if [ -z "$2" ]; then
+    USING_PEER_IN_ORG="0"
+  else
+    USING_PEER_IN_ORG=$2
+  fi
+
+
+  infoln "Using organization ${USING_ORG} and peer ${USING_PEER_IN_ORG}"
+  if [ $USING_ORG -eq 1 ]; then
+    export CORE_PEER_LOCALMSPID="Org1MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+    # podesavanje adrese peer-a na koji ce da se nakaci binary
+    if [ $USING_PEER_IN_ORG -eq 0 ]; then
+      export CORE_PEER_ADDRESS=localhost:7051
+    elif [ $USING_PEER_IN_ORG -eq 1 ]; then
+      export CORE_PEER_ADDRESS=localhost:8051
+    else
+      export CORE_PEER_ADDRESS=localhost:9051
+    fi
+
+  elif [ $USING_ORG -eq 2 ]; then
+    export CORE_PEER_LOCALMSPID="Org2MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG2_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+    if [ $USING_PEER_IN_ORG -eq 0 ]; then
+      export CORE_PEER_ADDRESS=localhost:10051
+    elif [ $USING_PEER_IN_ORG -eq 1 ]; then
+      export CORE_PEER_ADDRESS=localhost:11051
+    else
+      export CORE_PEER_ADDRESS=localhost:12051
+    fi
+
+  elif [ $USING_ORG -eq 3 ]; then
+    export CORE_PEER_LOCALMSPID="Org3MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG3_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org3.example.com/users/Admin@org3.example.com/msp
+    if [ $USING_PEER_IN_ORG -eq 0 ]; then
+      export CORE_PEER_ADDRESS=localhost:13051
+    elif [ $USING_PEER_IN_ORG -eq 1 ]; then
+      export CORE_PEER_ADDRESS=localhost:14051
+    else
+      export CORE_PEER_ADDRESS=localhost:15051
+    fi
+  else
+    errorln "ORG Unknown"
+  fi
+
+  if [ "$VERBOSE" == "true" ]; then
+    env | grep CORE
+  fi
+}
+
+# Set environment variables for use in the CLI container 
+setGlobalsCLI() {
+  setGlobals $1
+
+  local USING_ORG=""
+  if [ -z "$OVERRIDE_ORG" ]; then
+    USING_ORG=$1
+  else
+    USING_ORG="${OVERRIDE_ORG}"
+  fi
+  if [ $USING_ORG -eq 1 ]; then
+    export CORE_PEER_ADDRESS=peer0.org1.example.com:7051
+  elif [ $USING_ORG -eq 2 ]; then
+    export CORE_PEER_ADDRESS=peer0.org2.example.com:10051
+  elif [ $USING_ORG -eq 3 ]; then
+    export CORE_PEER_ADDRESS=peer0.org3.example.com:13051
+  else
+    errorln "ORG Unknown"
+  fi
+}
+
+# parsePeerConnectionParameters $@
+# Helper function that sets the peer connection parameters for a chaincode
+# operation
+parsePeerConnectionParameters() {
+  PEER_CONN_PARMS=()
+  PEERS=""
+  while [ "$#" -gt 0 ]; do
+    local ORG_NO=$1
+    local PEER_NO=$2
+    setGlobals $ORG_NO $PEER_NO
+    PEER="peer${PEER_NO}.org${ORG_NO}"
+    ## Set peer addresses
+    if [ -z "$PEERS" ]
+    then
+	PEERS="$PEER"
+    else
+	PEERS="$PEERS $PEER"
+    fi
+    PEER_CONN_PARMS=("${PEER_CONN_PARMS[@]}" --peerAddresses $CORE_PEER_ADDRESS)
+    ## Set path to TLS certificate
+    CA=PEER0_ORG$1_CA
+    TLSINFO=(--tlsRootCertFiles "${!CA}")
+    PEER_CONN_PARMS=("${PEER_CONN_PARMS[@]}" "${TLSINFO[@]}")
+    # shift by one to get to the next organization
+    shift
+    shift
+  done
+}
+
+verifyResult() {
+  if [ $1 -ne 0 ]; then
+    fatalln "$2"
+  fi
+}
+
+# parsePeerConnectionParameters 1 0 1 1 1 2 2 1 2 2 2 3 0 3 1 3 2
