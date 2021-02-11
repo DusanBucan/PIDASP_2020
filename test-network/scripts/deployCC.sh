ba0cccbb844c5a945e534077aa643e4485d8a95e
DusanBucan <dusanbzr@gmail.com>
2021-02-11 17:51:49 +0100

    adde from original test-net

--- test-network/scripts/deployCC.sh
+++ test-network/scripts/deployCC.sh
@@ -332,6 +332,8 @@ approveForMyOrg 1 0
 # approveForMyOrg 1 1
 # approveForMyOrg 1 2 
 
+
+##@TODO: ovde je pucalo kad sam dodao pa sam zakomentarisao, treba proveriti da li je neophodno
 ## check whether the chaincode definition is ready to be committed
 ## expect org1 to have approved and org2 not to
 checkCommitReadiness 1 0 "\"Org1MSP\": true" "\"Org2MSP\": false" "\"Org3MSP\": false"
